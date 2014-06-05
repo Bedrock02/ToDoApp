267002cd1ffafc730aa7ad8657d231f0369b87b7
Bedrock02 <JimSteve91@gmal.com>
2014-06-05 13:43:19 -0400

    ToDoApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToDoList::Application.config.secret_key_base = '637b03408df29170403d6457be876a1dcea53a1f3009b46b1084c63f539801fc4792e3fdfb80a7f8100e1b6662bbe9ac7bafa2fcc2a774eab5518fd2c8da9cf5'
