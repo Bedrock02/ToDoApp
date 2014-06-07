3e8039d2d96dc89dff8067a30dc9f6cd2ae0af4b
Bedrock02 <JimSteve91@gmal.com>
2014-06-07 13:25:57 -0400

    Configured permitted parameters for Devise Controller to allow phone number attribute to be added to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_filter :authenticate_user!
+
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_devise_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) << :phone_number
+  end
 end
