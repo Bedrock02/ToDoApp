de8d45b36bfcca305d1cc75b8940467e31d94903
Bedrock02 <JimSteve91@gmal.com>
2014-06-07 00:13:10 -0400

    Added helpers to reduce code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def mark_as_helper(status)
+		if status == "incomplete"
+			"Mark Complete"
+		else
+			"Mark Incomplete"
+		end
+	end
+
+	def no_tasks(user)
+		unless user.tasks.empty?
+			false
+		else
+			true
+		end
+	end
 end
