2e18a1b3d71765ce723ec4a02ee07a68844ea842
Bedrock02 <JimSteve91@gmal.com>
2014-06-08 21:48:46 -0400

    db schema adjusted

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605204406) do
+ActiveRecord::Schema.define(version: 20140607162315) do
 
   create_table "tasks", force: true do |t|
     t.string   "body"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140605204406) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
