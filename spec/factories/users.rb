fd6fea8bf5d146451559e37005748d04405a608b
Bedrock02 <JimSteve91@gmal.com>
2014-06-09 18:27:21 -0400

    removed unecessary code that included helpers, added the phone number field and changed the password to a valid entry

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-include Devise::TestHelpers
-
 FactoryGirl.define do
   factory :user do
   	email "example@example.com"
-  	password "example"
-  	password_confirmation "example"
+  	phone_number "555-555-5555"
+  	password "thisisareallybadpassowrd"
+  	password_confirmation "thisisareallybadpassowrd"
   end
 end
