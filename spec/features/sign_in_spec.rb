58197dd95afa812ed25fe4e919ace5d9548f294d
Bedrock02 <JimSteve91@gmal.com>
2014-06-09 18:33:16 -0400

    Specs used to test when a user signs into the application

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+describe "Logging in a User" do
+	
+	before(:each) do
+		@user = FactoryGirl.create(:user)
+	end
+	
+	context "User is able to log into the application" do
+		it "Shows user's ToDo List" do		
+			visit '/'
+			within('#new_user') do
+				fill_in 'Email', :with => @user.email
+				fill_in 'Password', :with => @user.password
+			end
+			click_button('Sign in')
+
+			expect(page).to have_content 'ToDo List'
+		end
+	end
+
+	context "User enters wrong credentials" do
+		it "Does not log in user" do
+			visit '/'
+			within('#new_user') do
+				fill_in 'Email', :with => @user.email
+				fill_in 'Password', :with => "notmypassword"
+			end
+			click_button('Sign in')
+
+			expect(page).to have_content 'Sign in'
+			within('#new_user'){
+				expect(page).to have_content 'Email'
+				expect(page).to have_content 'Password'
+			}
+		end
+	end
+end
