1020c095499efa10259bef2de763005d7149dd12
Bedrock02 <JimSteve91@gmal.com>
2014-06-10 10:52:48 -0400

    new db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607162315) do
+ActiveRecord::Schema.define(version: 20140610144428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sms_logs", force: true do |t|
+    t.string   "from"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "body"
