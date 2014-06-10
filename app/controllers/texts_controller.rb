ea0f90c8c9ecf75fc328bb6a234d878bce1b46dd
Bedrock02 <JimSteve91@gmal.com>
2014-06-10 12:12:15 -0400

    When users text in their task, a new task will be created for them

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -43,12 +43,28 @@ class TextsController < ApplicationController
 		text_body = params[:Body]
 		from_number = params[:From]
 
+		create_task_for_user(from_number,text_body)
+		
 		@new_log = SmsLog.new(body: text_body, from: from_number)
 		@new_log.save
 		
 		response = Twilio::TwiML::Response.new do |r|
-			r.Message "Hi there it finally worked."
+			r.Message "You have just texted in your new Task. We will update your profile for you."
 		end
 		render_twiml(response)
+	end
+
+	def create_task_for_user(number,text)
+		modified_format = number[2..-1]
+		modified_format.insert(3,"-")
+		modified_format.insert(7,"-")
+		user = User.find_by_phone_number(modified_format)
+
+		unless user.nil?
+			task = Task.new(body:text)
+			task.status_incomplete
+			task.user = user
+			task.save
+		end 
 	end
 end
