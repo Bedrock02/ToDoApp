c0fca96dba6cebf8378863ad06db38e057e5fff5
Bedrock02 <JimSteve91@gmal.com>
2014-06-10 10:36:43 -0400

    Testing Twilio API

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,3 +1,21 @@
+# {
+#   "AccountSid"=>"<string>", 
+#   "Body"=>"the message content", 
+#   "ToZip"=>"<string>", 
+#   "FromState"=>"<string>", 
+#   "ToCity"=>"<string>", 
+#   "SmsSid"=>"<string>", 
+#   "ToState"=>"<string>", 
+#   "To"=>"<string>", 
+#   "ToCountry"=>"<string>", 
+#   "FromCountry"=>"<string>", 
+#   "SmsMessageSid"=>"<string>", 
+#   "ApiVersion"=>"<string>", 
+#   "FromCity"=>"<string>", 
+#   "SmsStatus"=>"<string>", 
+#   "From"=>"<string>", 
+#   "FromZip"=>"<string>"
+# }
 
 class TextsController < ApplicationController
 	include Webhookable
@@ -27,9 +45,20 @@ class TextsController < ApplicationController
 	end
 
 	def messaging
-		@twiml = Twilio::TwiML::Response.new do |r|
-			r.Message "Thank you for texting in"
-		end
-		render_twiml @twiml
+		
+		message_body = params["Body"]
+		from_number = params["From"]
+		from_zip = params["FromZip"]
+
+		account_sid = "AC3126b43bc8d57aa5750bc123c75aabab"
+		auth_token = "ab465cea4dd39ee5bd577eab69adce53"
+		@client = Twilio::REST::Client.new account_sid, auth_token
+
+
+		@client.account.messages.create(
+			:from => "+13473345437",
+			:to => from_number,
+			:body => "Wow did I just get a message from you? Is your zip #{from_zip}?"
+			)
 	end
 end
