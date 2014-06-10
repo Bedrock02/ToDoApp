5770c54fb501c8caba9ca6507319e62823e9f78a
Bedrock02 <JimSteve91@gmal.com>
2014-06-10 18:35:49 -0400

    Refactored spec

--- spec/features/to_do_list_spec.rb
+++ spec/features/to_do_list_spec.rb
@@ -7,11 +7,11 @@ describe "ToDo List" do
 	before(:each) do
 		@user = FactoryGirl.create(:user)
 		login_as(@user, :scope => :user)
+		visit '/'
 	end
 
 	context "New user logged into account" do
 		it "should not display any tasks" do
-			visit '/'
 			expect(page).to have_no_css(".to-do-tasks")
 			expect(page).to have_content 'You currently do not have any Tasks in your ToDoList!'
 		end
@@ -30,5 +30,15 @@ describe "ToDo List" do
 			end
 		end
 	end
-	Warden.test_reset!
+
+	context "User can create a new task" do
+		it "should display task" do
+			click_button "New Task"
+			fill_in "task[body]", with: "blah blah blah"
+			click_button "Create Task"
+
+			expect(page).to have_content "blah blah blah"
+		end
+	end
 end
+Warden.test_reset!
