498c45c94b3cffe975b82bf46f656b67c096ba49
Bedrock02 <JimSteve91@gmal.com>
2014-06-10 12:33:51 -0400

    removed unecessary information

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,43 +1,8 @@
-# {
-#   "AccountSid"=>"<string>", 
-#   "Body"=>"the message content", 
-#   "ToZip"=>"<string>", 
-#   "FromState"=>"<string>", 
-#   "ToCity"=>"<string>", 
-#   "SmsSid"=>"<string>", 
-#   "ToState"=>"<string>", 
-#   "To"=>"<string>", 
-#   "ToCountry"=>"<string>", 
-#   "FromCountry"=>"<string>", 
-#   "SmsMessageSid"=>"<string>", 
-#   "ApiVersion"=>"<string>", 
-#   "FromCity"=>"<string>", 
-#   "SmsStatus"=>"<string>", 
-#   "From"=>"<string>", 
-#   "FromZip"=>"<string>"
-# }
-
-# account_sid = "AC3126b43bc8d57aa5750bc123c75aabab"
-# 		auth_token = "ab465cea4dd39ee5bd577eab69adce53"
-# 		@client = Twilio::REST::Client.new account_sid, auth_token
-
-
-# 		@client.account.messages.create(
-# 			:from => "+13473345437",
-# 			:to => "+16467031728",
-# 			:body => params[:q]
-# 			)
-
-
 class TextsController < ApplicationController
 	include Webhookable
 	
 	after_filter :set_header
-
 	skip_before_action :verify_authenticity_token
-
-	def index
-	end
 
 	def messaging
 		text_body = params[:Body]
