1cefe69172e58dc7110a18fb407c480590d1a552
Bedrock02 <JimSteve91@gmal.com>
2014-06-10 16:46:07 -0400

    changed the email for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 FactoryGirl.define do
   factory :user do
-  	email "example@example.com"
+  	email "example1@example.com"
   	phone_number "555-555-5555"
   	password "thisisareallybadpassowrd"
   	password_confirmation "thisisareallybadpassowrd"
