462eb28a3f07d03c5e052a71dd1f2606a5f10d04
Bedrock02 <JimSteve91@gmal.com>
2014-06-11 11:38:07 -0400

    Changed the redirect to point to the index rather than the show for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
